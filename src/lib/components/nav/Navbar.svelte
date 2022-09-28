1c86c8c2749a1f3e22f374e86d7664e1e2596ea7
recluse4615 <112841230+recluse4615@users.noreply.github.com>
2022-09-28 02:44:26 +0100

    adding "left" and "right" sides for navs adding cog icon for settings page

--- src/lib/components/nav/Navbar.svelte
+++ src/lib/components/nav/Navbar.svelte
@@ -1,5 +1,8 @@
-<div class="text-md text-center text-copy bg-darkest fixed top-0 z-10 w-full">
+<div class="text-md text-center text-copy bg-darkest fixed top-0 z-10 w-full flex flex-row justify-between">
 	<ul class="flex flex-wrap -mb-px text-neutral-400">
-		<slot />
+		<slot name="left"></slot>
+	</ul>
+	<ul class="flex flex-wrap -mb-px text-neutral-400">
+		<slot name="right"></slot>
 	</ul>
 </div>
