6b768485b8dda91d543d36f5506cc47bded39a65
recluse4615 <112841230+recluse4615@users.noreply.github.com>
2022-09-28 03:05:32 +0100

    adding new icons!! (#12)

--- src/lib/components/nav/Navbar.svelte
+++ src/lib/components/nav/Navbar.svelte
@@ -1,8 +1,20 @@
+<script>
+	import Logo from '$lib/icon/Logo.svelte';
+</script>
 <div class="text-md text-center text-copy bg-darkest fixed top-0 z-10 w-full flex flex-row justify-between">
-	<ul class="flex flex-wrap -mb-px text-neutral-400">
-		<slot name="left"></slot>
-	</ul>
-	<ul class="flex flex-wrap -mb-px text-neutral-400">
-		<slot name="right"></slot>
-	</ul>
+	<div class="flex flex-row">
+		<div class="w-12 mx-4 mt-1">
+			<Logo />
+		</div>
+
+		<ul class="flex flex-wrap -mb-px text-neutral-400">
+			<slot name="left"></slot>
+		</ul>
+	</div>
+	<div>
+		<ul class="flex flex-wrap -mb-px text-neutral-400">
+			<slot name="right"></slot>
+		</ul>
+	</div>
+
 </div>
