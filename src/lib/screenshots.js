0f0be3e410fc011a6adfd329865d7f985973b86e
Miniontoby <tobias.gaarenstroom@gmail.com>
2022-09-28 16:50:41 +0200

    Added All Screenshots Header and changed Cargo.toml

--- src/lib/screenshots.js
+++ src/lib/screenshots.js
@@ -1,18 +1,26 @@
 import { BaseDirectory, readDir, createDir } from "@tauri-apps/api/fs";
+import { getInstalledVersions } from '$lib/api/versions';
 
-export async function getScreenshots(version) {
-    try {
-        let entries = await readDir(`versions/${version}/screenshots`, {
-            dir: BaseDirectory.App,
-            recursive: false
-        });
-        return entries.sort((a,b) => a.lastModified - b.lastModified);
+export async function getScreenshots(version=null) {
+    let screenshots = [];
+    if(version != null) {
+        try {
+            screenshots = await readDir(`versions/${version}/screenshots`, {
+                dir: BaseDirectory.App,
+                recursive: false
+            });
+        }
+        catch {
+            await createDir(`versions/${version}/screenshots`, {
+                dir: BaseDirectory.App,
+                recursive: false
+            });
+            return [];
+        }
+    } else {
+        let versions = await getInstalledVersions();
+        for (let i in versions) 
+            screenshots = screenshots.concat(await getScreenshots(versions[i]));
     }
-    catch {
-        await createDir(`versions/${version}/screenshots`, {
-            dir: BaseDirectory.App,
-            recursive: false
-        });
-        return [];
-    }
+    return screenshots.sort((a,b) => a.lastModified - b.lastModified);
 } 
--- src/routes/screenshots/+page.svelte
+++ src/routes/screenshots/+page.svelte
@@ -8,7 +8,8 @@
     import FullLoader from '$lib/components/FullLoader.svelte';
 	import LocalImage from '$lib/components/image/LocalImage.svelte';
 
-    let screenshots = [];
+    let screenshots = [], 
+	allscreenshots = [];
     onMount(async() => {
 		selectedVersion.subscribe(async (val) => {
             if (!val.installed) {
@@ -18,23 +19,36 @@
 
 			screenshots = await getScreenshots(val.name);
         });
+		allscreenshots = await getScreenshots();
     });
 </script>
 <div class="pt-8">
 	{#if screenshots}
 		<div class="px-64">
-			{#if screenshots.length}
-				<div class="pb-16">
-					<h1 class="text-2xl font-bold pb-4">Screenshots</h1>
+			<div class="pb-16">
+				<h1 class="text-2xl font-bold pb-4">Screenshots for selected version</h1>
+				{#if screenshots.length}
 					<div class="grid grid-cols-2 gap-4">
 						{#each screenshots as screenshot}
 							<LocalImage src={screenshot.path} alt={screenshot.name}/>
 						{/each}
 					</div>
-				</div>
-			{:else}
-				<span>No screenshots</span>
-			{/if}
+				{:else}
+					<span>No screenshots</span>
+				{/if}
+			</div>
+			<div class="pb-16">
+				<h1 class="text-2xl font-bold pb-4">All Screenshots</h1>
+				{#if allscreenshots.length}
+					<div class="grid grid-cols-2 gap-4">
+						{#each allscreenshots as screenshot}
+							<LocalImage src={screenshot.path} alt={screenshot.name}/>
+						{/each}
+					</div>
+				{:else}
+					<span>No screenshots</span>
+				{/if}
+			</div>
 		</div>
 	{:else}
 		<FullLoader />
